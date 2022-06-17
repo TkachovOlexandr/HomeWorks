19a214bf6d9e18390e8021a6e4174a865fb8b3a2
tigrjilia <Tkac_el43@student.itstep.org>
2022-06-17 18:28:25 +0200

    The first task had been added

--- homework_1 (18.06.2022)/Program.cs
+++ homework_1 (18.06.2022)/Program.cs
@@ -6,7 +6,22 @@ namespace homework_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exercise #1
+            Console.Write("Write a number from 1 to 100: ");
+            short num = short.Parse(Console.ReadLine());
+            if (num > 0 && num < 101)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                    Console.WriteLine("Fizz Buzz");
+                else if (num % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (num % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(num);
+            }
+            else
+                Console.WriteLine("Your number doesn't match the specified range");
         }
     }
 }
