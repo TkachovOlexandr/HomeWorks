3da43dcf82026cdaa71b20bb4a175ef3fff1c952
tigrjilia <Tkac_el43@student.itstep.org>
2022-06-23 21:39:41 +0200

    I made 3 exercises

--- homework_2 (25.06.2022)/Program.cs
+++ homework_2 (25.06.2022)/Program.cs
@@ -1,101 +1,185 @@
 ﻿//Exercise #1
-Random random = new Random();
-int min_random = 1;
-int max_random = 100;
-short A_size = 5;
-short[] A = new short[A_size];
-short B_size_r = 3;
-short B_size_c = 4;
-float[,] B = new float[B_size_r, B_size_c];
-for (short i = 1; i <= A_size; i++)
-{
-    Console.Write("Write the " + i);
-    if (i == 1)
-        Console.Write("-st");
-    else if (i == 2)
-        Console.Write("-nd");
-    else if (i == 3)
-        Console.Write("-rd");
-    else
-        Console.Write("-th");
-    Console.Write(" number of array: ");
-    A[i - 1] = short.Parse(Console.ReadLine());
-}
-for (int i = 0; i < B_size_r; i++)
-{
-    for (int j = 0; j < B_size_c; j++)
-        B[i, j] = (float)random.Next(min_random, max_random) / 10;
-}
-short A_max = 0, A_min = 0, A_sum = 0, A_even_elem_sum = 0, A_product = 1;
-float B_max = 0, B_min = 0, B_sum = 0, B_odd_elem_sum = 0, B_product = 1;
-for (int i = 0; i < A_size; i++)
-{
-    Console.Write($"{A[i]}\t");
-    if (i == 0)
-        A_max = A[i];
-    else
-    {
-        if (A_max > A[i])
-            A_max *= 1;
-        else
-            A_max = A[i];
-    }
-    if (i == 0)
-        A_min = A[i];
-    else
-    {
-        if (A_min < A[i])
-            A_min *= 1;
-        else
-            A_min = A[i];
-    }
-    A_sum += A[i];
-    A_product *= A[i];
-    if (i % 2 == 1)
-        A_even_elem_sum += A[i];
-}
-Console.WriteLine();
-for (int i = 0; i < B_size_r; i++)
-{
-    for (int j = 0; j < B_size_c; j++)
-    {
-        Console.Write($"{B[i, j]}\t");
-        if (i == 0 && j == 0)
-            B_max = B[i, j];
-        else
-        {
-            if (B_max > B[i, j])
-                B_max *= 1;
-            else
-                B_max = B[i, j];
-        }
-        if (i == 0 && j == 0)
-            B_min = B[i, j];
-        else
-        {
-            if (B_min < B[i, j])
-                B_min *= 1;
-            else
-                B_min = B[i, j];
-        }
-        B_sum += B[i, j];
-        B_product *= B[i, j];
-        if (j % 2 == 0)
-            B_odd_elem_sum += B[i, j];
-    }
-    Console.WriteLine();
-}
-Console.Write($"Joint max element: ");
-if (A_max > B_max)
-    Console.WriteLine(A_max);
-else
-    Console.WriteLine(B_max);
-Console.Write($"Joint min element: ");
-if (A_min < B_min)
-    Console.WriteLine(A_min);
-else
-    Console.WriteLine(B_min);
-Console.WriteLine($"Sum of all elements: {A_sum + B_sum}");
-Console.WriteLine($"Product of all elements: {A_product * B_product}");
-Console.WriteLine($"Sum of even elements from array A: {A_even_elem_sum}");
-Console.WriteLine($"Sum of odd elements from array B: {B_odd_elem_sum}");
+//Random random = new Random();
+//int min_random = 1;
+//int max_random = 100;
+//short A_size = 5;
+//short[] A = new short[A_size];
+//short B_size_r = 3;
+//short B_size_c = 4;
+//float[,] B = new float[B_size_r, B_size_c];
+//for (short i = 1; i <= A_size; i++)
+//{
+//    Console.Write("Write the " + i);
+//    if (i == 1)
+//        Console.Write("-st");
+//    else if (i == 2)
+//        Console.Write("-nd");
+//    else if (i == 3)
+//        Console.Write("-rd");
+//    else
+//        Console.Write("-th");
+//    Console.Write(" number of array: ");
+//    A[i - 1] = short.Parse(Console.ReadLine());
+//}
+//for (int i = 0; i < B_size_r; i++)
+//{
+//    for (int j = 0; j < B_size_c; j++)
+//        B[i, j] = (float)random.Next(min_random, max_random) / 10;
+//}
+//short A_max = 0, A_min = 0, A_sum = 0, A_even_elem_sum = 0, A_product = 1;
+//float B_max = 0, B_min = 0, B_sum = 0, B_odd_elem_sum = 0, B_product = 1;
+//for (int i = 0; i < A_size; i++)
+//{
+//    Console.Write($"{A[i]}\t");
+//    if (i == 0)
+//        A_max = A[i];
+//    else
+//    {
+//        if (A_max > A[i])
+//            A_max *= 1;
+//        else
+//            A_max = A[i];
+//    }
+//    if (i == 0)
+//        A_min = A[i];
+//    else
+//    {
+//        if (A_min < A[i])
+//            A_min *= 1;
+//        else
+//            A_min = A[i];
+//    }
+//    A_sum += A[i];
+//    A_product *= A[i];
+//    if (i % 2 == 1)
+//        A_even_elem_sum += A[i];
+//}
+//Console.WriteLine();
+//for (int i = 0; i < B_size_r; i++)
+//{
+//    for (int j = 0; j < B_size_c; j++)
+//    {
+//        Console.Write($"{B[i, j]}\t");
+//        if (i == 0 && j == 0)
+//            B_max = B[i, j];
+//        else
+//        {
+//            if (B_max > B[i, j])
+//                B_max *= 1;
+//            else
+//                B_max = B[i, j];
+//        }
+//        if (i == 0 && j == 0)
+//            B_min = B[i, j];
+//        else
+//        {
+//            if (B_min < B[i, j])
+//                B_min *= 1;
+//            else
+//                B_min = B[i, j];
+//        }
+//        B_sum += B[i, j];
+//        B_product *= B[i, j];
+//        if (j % 2 == 0)
+//            B_odd_elem_sum += B[i, j];
+//    }
+//    Console.WriteLine();
+//}
+//Console.Write($"Joint max element: ");
+//if (A_max > B_max)
+//    Console.WriteLine(A_max);
+//else
+//    Console.WriteLine(B_max);
+//Console.Write($"Joint min element: ");
+//if (A_min < B_min)
+//    Console.WriteLine(A_min);
+//else
+//    Console.WriteLine(B_min);
+//Console.WriteLine($"Sum of all elements: {A_sum + B_sum}");
+//Console.WriteLine($"Product of all elements: {A_product * B_product}");
+//Console.WriteLine($"Sum of even elements from array A: {A_even_elem_sum}");
+//Console.WriteLine($"Sum of odd elements from array B: {B_odd_elem_sum}");
+
+//Exercise #2
+//Random random = new Random();
+//int min_random = -100;
+//int max_random = 100;
+//short size_r = 5;
+//short size_c = 5;
+//short[,] arr = new short[size_r, size_c];
+//for (int i = 0; i < size_r; i++)
+//{
+//    for (int j = 0; j < size_c; j++)
+//        arr[i, j] = (short)random.Next(min_random, max_random);
+//}
+//short min_value = 0, max_value = 0;
+//for (short i = 0; i < size_r; i++)
+//{
+//    for (short j = 0; j < size_c; j++)
+//    {
+//        if (i == 0 && j == 0)
+//            max_value = arr[i, j];
+//        else if (max_value < arr[i, j])
+//            max_value = arr[i, j];
+//        if (i == 0 && j == 0)
+//            min_value = arr[i, j];
+//        else if (min_value > arr[i, j])
+//            min_value = arr[i, j];
+//        Console.Write($"{arr[i, j]}\t");
+//    }
+//    Console.WriteLine();
+//}
+//short[] left_board = new short[2];
+//short[] right_board = new short[2];
+//for (short i = 0; i < size_r; i++)
+//{
+//    for (short j = 0; j < size_c; j++)
+//    {
+//        if (arr[i, j] == max_value || arr[i, j] == min_value)
+//        {
+//            right_board[0] = i;
+//            right_board[1] = j;
+//        }
+//    }
+//}
+//for (short i = (short)(size_r - 1); i >= 0; i--)
+//{
+//    for (short j = (short)(size_c - 1); j >= 0; j--)
+//    {
+//        if (arr[i, j] == max_value || arr[i, j] == min_value)
+//        {
+//            left_board[0] = i;
+//            left_board[1] = j;
+//        }
+//    }
+//}
+//short sum = 0;
+//for (short i = left_board[0]; i <= right_board[0]; i++)
+//{
+//    for (short j = left_board[1]; j <= right_board[1]; j++)
+//        sum += arr[i, j];
+//}
+//Console.Write("Sum of elements between ");
+//if (arr[left_board[0], left_board[1]] == min_value)
+//    Console.Write("min");
+//else
+//    Console.Write("max");
+//Console.Write($" {arr[left_board[0], left_board[1]]} and ");
+//if (arr[right_board[0], right_board[1]] == min_value)
+//    Console.Write("min");
+//else
+//    Console.Write("max");
+//Console.WriteLine($" {arr[right_board[0], right_board[1]]}: {sum}");
+
+//Exercise #3
+//Console.Write("Write your string: ");
+//string person_str = Console.ReadLine();
+//Console.Write("Write the number of movements: ");
+//short num = short.Parse(Console.ReadLine());
+//char[] char_arr = person_str.ToCharArray();
+//for (short i = 0; i < person_str.Length; i++)
+//    char_arr[i] = (char)(Convert.ToInt16(person_str[i]) + num);
+//string answer_str = new string(char_arr);
+//Console.WriteLine($"Your encrypted string: {answer_str}");
+
+//Exercise #4
